28ff80a067e8442862d48addbfa3c98dfc2810d9
German <v-german@pulumi.com>
2023-08-01 10:47:28 -0300

    update

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket");
+const bucket2 = new aws.s3.Bucket("my-second-bucket");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+export const secondBucketName = bucket2.id;
