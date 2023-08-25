5e3b3b4ce900af943a8da12af2e3180a417932a5
German <v-german@pulumi.com>
2023-08-25 11:52:38 -0300

    update

--- index.ts
+++ index.ts
@@ -1,53 +1,35 @@
-import * as pulumi from "@pulumi/pulumi";
+// import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
-import * as pulumiservice from "@pulumi/pulumiservice";
+// import * as awsx from "@pulumi/awsx";
+// import * as pulumiservice from "@pulumi/pulumiservice";
 
-const config = new pulumi.Config("aws");
+// const config = new pulumi.Config("aws");
 
-const deploymentSettings = new pulumiservice.DeploymentSettings("deploymentSettings", {
-    operationContext: {
-        environmentVariables: {
-            "aws:region": "us-east-2",
-        },
-        options: {},
-    },
-    organization: "german",
-    project: "german-test",
-    sourceContext: {
-        git: {
-            branch: "refs/heads/main",
-            repoDir: ".",
-            repoUrl: "https://github.com/glena/test-deployment",
-        },
-    },
-    stack: "dev",
-}, {
-    protect: true,
-});
+// const deploymentSettings = new pulumiservice.DeploymentSettings("deploymentSettings", {
+//     operationContext: {
+//         environmentVariables: {
+//             "aws:region": "us-east-2",
+//         },
+//         options: {},
+//     },
+//     organization: "german",
+//     project: "german-test",
+//     sourceContext: {
+//         git: {
+//             branch: "refs/heads/main",
+//             repoDir: ".",
+//             repoUrl: "https://github.com/glena/test-deployment",
+//         },
+//     },
+//     stack: "dev",
+// }, {
+//     protect: true,
+// });
 
 // // Create an AWS resource (S3 Bucket)
-// const bucket = new aws.s3.Bucket("my-first-bucket");
-// const bucket2 = new aws.s3.Bucket("my-second-bucket");
+const bucket = new aws.s3.Bucket("my-first-bucket");
+const bucket2 = new aws.s3.Bucket("my-second-bucket");
 
 // // Export the name of the bucket
-// export const bucketName = bucket.id;
-// export const secondBucketName = bucket2.id;
-
-// const webhook = new pulumiservice.Webhook("example-webhook", {
-//     active: true,
-//     displayName: "german webhook example",
-//     organizationName: "pulumi",
-//     projectName: "german-test",
-//     stackName: "dev",
-//     payloadUrl: "https://example.com/webhook",
-// });
-
-// const team = new pulumiservice.Team("german-test-team", {
-//     organizationName: "pulumi",
-//     displayName: "german test team",
-//     description: "german test team",
-//     teamType: "pulumi",
-//     name: "german-test-team",
-//     members: ["glena"]
-// });
+export const bucketName = bucket.id;
+export const secondBucketName = bucket2.id;
