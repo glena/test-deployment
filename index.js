7200c9b347c270dadb15826aac9f74707b1f65c3
German <v-german@pulumi.com>
2023-12-01 08:57:16 -0300

    new

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
-// import * as pulumi from "@pulumi/pulumi";
-// import * as aws from "@pulumi/aws";
-// import * as awsx from "@pulumi/awsx";
-// import * as pulumiservice from "@pulumi/pulumiservice";
+import * as aws from "@pulumi/aws";
 
-// const config = new pulumi.Config("aws");
+console.log('Look ma I am running!');
 
-// const deploymentSettings = new pulumiservice.DeploymentSettings("deploymentSettings", {
-//     operationContext: {
-//         environmentVariables: {
-//             "aws:region": "us-east-2",
-//         },
-//         options: {},
-//     },
-//     organization: "german",
-//     project: "german-test",
-//     sourceContext: {
-//         git: {
-//             branch: "refs/heads/main",
-//             repoDir: ".",
-//             repoUrl: "https://github.com/glena/test-deployment",
-//         },
-//     },
-//     stack: "dev",
-// }, {
-//     protect: true,
-// });
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.Bucket("my-first-bucket");
 
-// // Create an AWS resource (S3 Bucket)
-// const bucket = new aws.s3.Bucket("my-first-bucket");
-// const bucket2 = new aws.s3.Bucket("my-second-bucket");
+// // Export the name of the bucket
+export const bucketName = bucket.id;
 
-// // // Export the name of the bucket
-// export const bucketName = bucket.id;
-// export const secondBucketName = bucket2.id;
-console.log('Look ma I am running');
+console.log('Done!');
