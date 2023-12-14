7335785ce853abf75cf5c7ffb98f8f5115ef481b
Germán Lena <v-german@pulumi.com>
2023-12-14 11:26:58 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
-import * as aws from "@pulumi/aws";
+// import * as aws from "@pulumi/aws";
 
-const bucket = new aws.s3.Bucket("my-bucket", {
-    acl: "private",
-    tags: {
-        Environment: "Dev",
-        Name: "My bucket",
-    },
-});
+// const bucket = new aws.s3.Bucket("my-bucket", {
+//     acl: "private",
+//     tags: {
+//         Environment: "Dev",
+//         Name: "My bucket",
+//     },
+// });
 
-export const bucketName = bucket.id;
+// export const bucketName = bucket.id;
+
+console.log(`LOOK MA I'M RUNNING`);
+console.log(process.env);
