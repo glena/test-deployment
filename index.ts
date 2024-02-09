7312825cd1f825dfef31c3113bd57dedb2c354bc
Germán Lena <v-german@pulumi.com>
2024-02-09 13:38:59 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -16,6 +16,4 @@ console.log('SOME_LOCAL_ENV_VAR', process.env['SOME_LOCAL_ENV_VAR']);
 console.log('SETTINGS_VAR_1', process.env['SETTINGS_VAR_1']);
 console.log('SETTINGS_VAR_2_SECRET', process.env['SETTINGS_VAR_2_SECRET']);
 
-
-
 export const someOutput = "the value";
