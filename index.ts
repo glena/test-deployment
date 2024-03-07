8232be21d6f3c98637d969c3f0565a508daad0ad
German <v-german@pulumi.com>
2024-03-07 13:36:57 -0300

    new outputs

--- index.ts
+++ index.ts
@@ -11,13 +11,9 @@
 
 // export const bucketName = bucket.id;
 
-console.log(`LOOK MA I'M RUNNING`);
-console.log(process.env);
-console.log('SOME_LOCAL_ENV_VAR', process.env['SOME_LOCAL_ENV_VAR']);
-console.log('SETTINGS_VAR_1', process.env['SETTINGS_VAR_1']);
-console.log('SETTINGS_VAR_2_SECRET', process.env['SETTINGS_VAR_2_SECRET']);
 
-
-
+export const someOutput2 = "the value";
+export const someOutput3 = "the value";
+export const someOutput4 = "the value";
 
 export const someOutput = "the value";
